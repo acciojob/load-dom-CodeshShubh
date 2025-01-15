6308f5d161ffe61f706746ca75d08f7a9a63cf7f
Shubhanshu Saadhiyaan <138874847+CodeshShubh@users.noreply.github.com>
2025-01-15 16:02:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,5 +3,5 @@
 document.addEventListener('DOMContentLoaded', function(){
 var newElement = document.createElement("h1");
      newElement.textContent = "DOM load success";
-     document.body.appendChild(newElement);
+     document.body.appendChild(newElement); 
 }) 
